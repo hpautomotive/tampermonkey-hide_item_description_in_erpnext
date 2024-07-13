4c6347d315d41bc331d841223beb5a5ab9263c84
hpautomotive <156674292+hpautomotive@users.noreply.github.com>
2024-07-13 11:54:19 +0530

    Update Auto Set Print Settings.js

--- Auto Set Print Settings.js
+++ Auto Set Print Settings.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name         Auto Set Print Settings
 // @namespace    http://tampermonkey.net/
-// @version      1.1.0
+// @version      1.1.1
 // @description  Automatically sets the print orientation to Portrait, checks the "With Letterhead" checkbox, and sets the correct Letter Head based on the company name. Additionally, handles column selection based on company name.
 // @author       H.P. AUTOMOTIVE
-// @match        https://erpnext.hpagroup.co.in/app/*
+// @match        https://erpnext.hpagroup.co.in/app/query-report/*
 // @grant        none
 // @updateURL    https://raw.githubusercontent.com/hpautomotive/tampermonkey-hide_item_description_in_erpnext/main/Auto%20Set%20Print%20Settings.js
 // @downloadURL  https://raw.githubusercontent.com/hpautomotive/tampermonkey-hide_item_description_in_erpnext/main/Auto%20Set%20Print%20Settings.js
